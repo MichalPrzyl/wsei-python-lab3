b472c69566225a280657b22f3ff26c55cc19843a
Michał Przyłucki <michal.przyl@gmail.com>
2023-10-29 12:38:01 +0100

    zad 5

---
+++ zad5.py
@@ -0,0 +1,22 @@
+# Zadanie 5
+# Proszę zaimplementować klasę Student. Proszę zaimplementować następujące właściwości jako
+# prywatne: name, roll_number. Następnie należy zaimplementować następujące metody, aby uzyskać
+# i ustawić powyższe właściwości prywatne: getName(), setName(), getRollNumber(), setRollNumber()
+
+
+class Student:
+    def __init__(self):
+        self.__name = ''
+        self.__roll_number = ''
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, value):
+        self.__name = value
+
+    def get_roll_number(self):
+        return self.__roll_number
+
+    def set_roll_number(self, value):
+        self.__roll_number = value
